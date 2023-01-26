cf380f74d5bdcc4abc5bfad07585fd9ef39140b3
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-01-26 11:45:45 +0800

    Add link to payment

--- Views/(4) Payment/payment.php
+++ Views/(4) Payment/payment.php
@@ -7,16 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Checkout Menu</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <link rel="stylesheet" href="../../Styles (css)/style.css">
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
-        integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
 
-    <link href="https://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/favicon.ico" rel="shortcut icon"
-        type="image/x-icon">
+    <link href="https://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
 
 </head>
 
@@ -90,22 +87,17 @@
             </div>
             <div class="col-md-7" id="searchbar">
                 <div class="input-group mt-3">
-                    <input type="text" class="form-control" placeholder="Search" aria-label="Search"
-                        aria-describedby="button-addon2">
+                    <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
                     <div class="input-group-append">
-                        <button class="btn btn-primary" type="button" id="button-addon2"><i
-                                class="fas fa-search"></i></button>
+                        <button class="btn btn-primary" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
                     </div>
                 </div>
             </div>
             <div class="col-md-3 mt-2" id="icons">
-                <div style="cursor: pointer;" class="circle float-right" data-toggle="modal" data-target="#loginModal"
-                    data-toggle="tooltip" data-placement="top" title="LOGIN & SIGNUP"><i
-                        class="fas fa-user text-primary"></i></div>
+                <div style="cursor: pointer;" class="circle float-right" data-toggle="modal" data-target="#loginModal" data-toggle="tooltip" data-placement="top" title="LOGIN & SIGNUP"><i class="fas fa-user text-primary"></i></div>
 
                 <a href="cart.html">
-                    <div class="circle float-right mr-2"><i class="fas fa-shopping-cart text-primary"></i><sup><span
-                                class="badge badge-danger" style="width: 23px;">0</span></sup></div>
+                    <div class="circle float-right mr-2"><i class="fas fa-shopping-cart text-primary"></i><sup><span class="badge badge-danger" style="width: 23px;">0</span></sup></div>
                 </a>
             </div>
         </div>
@@ -115,8 +107,7 @@
     <div class="container" style="height: 30px;">
         <nav class="navbar navbar-expand-lg navbar-light" style="height: 30px;">
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -125,8 +116,7 @@
 
                     <li class="nav-item dropdown">
 
-                        <a class="nav-link dropdown" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                            aria-expanded="false"> <span> ≡ All Category</span></a>
+                        <a class="nav-link dropdown" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"> <span> ≡ All Category</span></a>
 
                         <div class="dropdown-menu">
                             <a class="dropdown-item" href="#">Comb Bind Notebook</a>
@@ -140,8 +130,7 @@
 
                     <li class="nav-item dropdown">
 
-                        <a class="nav-link dropdown" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                            aria-expanded="false">
+                        <a class="nav-link dropdown" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                             Campus
                         </a>
 
@@ -181,7 +170,7 @@
 
                     //select query
                     $query = mysqli_query($mysql, "SELECT id FROM users WHERE id = 1"); //execute the query
-                    
+
                     $row = mysqli_fetch_assoc($query);
 
                     foreach ((array) $row as $row) {
@@ -192,7 +181,7 @@
 
                     <?php
                     while ($row2 = mysqli_fetch_assoc($mysql2)) {
-                        ?>
+                    ?>
 
                         <tr>
                             <input type="hidden" name="id" value="<?= $row2['id']; ?>">
@@ -205,8 +194,7 @@
                         <br>
                         <tr>
                             <td><label for="phoneNo">Mobile number:</label></td>
-                            <td><input type="text" value="<?= $row2['mobile_no'] ?>" name="mobile_no" maxlength="7"
-                                    size="7">
+                            <td><input type="text" value="<?= $row2['mobile_no'] ?>" name="mobile_no" maxlength="7" size="7">
                             </td>
                         </tr>
                         <br>
@@ -223,7 +211,7 @@
                         </tr>
                         <br>
 
-                        <?php
+                    <?php
                     }
                     ?>
 
@@ -248,7 +236,7 @@
 
                     ?>
 
-                    <form action="payment1.php" method="post">
+                    <form action="payment.php" method="post">
                         <label for="">Payment Method : </label>
                         <select name="name" id="name">
                             <?php
@@ -258,9 +246,9 @@
                                 while ($optionData = $result->fetch_assoc()) {
                                     $option = $optionData['name'];
 
-                                    ?>
+                            ?>
                                     <option value="<?php echo $id; ?>"><?php echo $option; ?> </option>
-                                    <?php
+                            <?php
                                 }
                             }
                             ?>
@@ -299,9 +287,9 @@
                                 while ($optionData = $result->fetch_assoc()) {
                                     $option = $optionData['name'];
 
-                                    ?>
+                            ?>
                                     <option value="<?php echo $id; ?>"><?php echo $option; ?> </option>
-                                    <?php
+                            <?php
                                 }
                             }
                             ?>
@@ -329,7 +317,7 @@
 
                             $total_price = $row["total_price"];
 
-                            ?>
+                    ?>
 
                             Total :
                             <?php echo $total_price; ?><br>
@@ -338,13 +326,10 @@
 
 
 
-                            <?php
+                    <?php
                         }
                     } else {
                         echo "0 results";
-
-
-
                     }
                     ?>
 
@@ -392,15 +377,9 @@
 
 </body>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-    crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-    crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="js/script.js"></script>
 
 </html>
